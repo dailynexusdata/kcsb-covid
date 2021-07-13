b9349874c9077ca7af24c44cca789a531fd51c22
alrudolph <alex3r@me.com>
2021-07-12 18:54:56 -0700

    touch events

--- plots/countyVaccines.js
+++ plots/countyVaccines.js
@@ -5,7 +5,8 @@
 
   const data = (
     await d3.csv(
-      "https://raw.githubusercontent.com/dailynexusdata/kcsb-covid/main/data/county_vaccines.csv"
+      "data/county_vaccines.csv"
+      // "https://raw.githubusercontent.com/dailynexusdata/kcsb-covid/main/data/county_vaccines.csv"
     )
   ).map(({ date, fullPct, singlePct }) => {
     return {
@@ -20,219 +21,233 @@
     Full: "#4E79A7",
   };
 
-  const size = {
-    width: Math.min(600, window.innerWidth * 0.95),
-    height: 400,
-  };
+  window.addEventListener("resize", () => {
+    makePlot();
+  });
 
-  const margin = {
-    top: 30,
-    bottom: 40,
-    left: 5,
-    right: 50,
-  };
+  const makePlot = () => {
+    const size = {
+      width: Math.min(600, window.innerWidth * 0.95),
+      height: 400,
+    };
 
-  const svg = d3.select("#vaccinePlot");
+    const margin = {
+      top: 30,
+      bottom: 40,
+      left: window.innerWidth > 500 ? 50 : 15,
+      right: 50,
+    };
 
-  svg.attr("width", size.width).attr("height", size.height);
+    const svg = d3.select("#vaccinePlot");
 
-  const x = d3
-    .scaleTime()
-    .domain([data[0].date, data[data.length - 1].date])
-    .range([margin.left, size.width - margin.right]);
+    svg.selectAll("*").remove();
 
-  svg
-    .append("g")
-    .style("font-family", "Helvetica Neue,Helvetica,Arial,sans-serif")
-    .style("font-size", "12pt")
-    .attr("transform", `translate(0, ${size.height - margin.bottom + 5})`)
-    .call(d3.axisBottom().scale(x).tickFormat(d3.timeFormat("%B")));
+    svg.attr("width", size.width).attr("height", size.height);
 
-  const y = d3
-    .scaleLinear()
-    .domain([0, 0.6])
-    .range([size.height - margin.bottom, margin.top]);
+    const x = d3
+      .scaleTime()
+      .domain([data[0].date, data[data.length - 1].date])
+      .range([margin.left, size.width - margin.right]);
 
-  const horizLines = svg.append("g");
-
-  y.ticks(5)
-    .slice(1)
-    .forEach((yVal, i) => {
-      horizLines
-        .append("text")
-        .text(`${yVal * 100}%${i === 5 ? " of Total County Population" : ""}`)
-        .attr("fill", "#adadad")
-        .attr("x", margin.left)
-        .attr("y", y(yVal) - 5);
-
-      horizLines
-        .append("line")
-        .attr("x1", margin.left)
-        .attr("x2", size.width - margin.right)
-        .attr("y1", y(yVal))
-        .attr("y2", y(yVal))
-        .attr("stroke", "#d3d3d3")
-        .attr("stroke-width", "0.5px");
-    });
-
-  const singleArea = d3
-    .area()
-    .x((d) => x(d.date))
-    .y0((d) => y(d.fullPct))
-    .y1((d) => y(d.singlePct));
-
-  const singleLine = d3
-    .line()
-    .x((d) => x(d.date))
-    .y((d) => y(d.singlePct));
-  svg
-    .append("g")
-    .datum(data)
-    .append("path")
-    .attr("d", singleArea)
-    .attr("class", "vaccineArea")
-    .attr("id", "vaccine-partial")
-    .attr("fill", color.Partial)
-    .attr("fill-opacity", 0.5);
-
-  svg
-    .append("g")
-    .datum(data)
-    .append("path")
-    .attr("class", "vaccineLine")
-    .attr("id", "vaccineLine-partial")
-    .attr("d", singleLine)
-    .attr("stroke", color.Partial)
-    .attr("stroke-width", 5)
-    .attr("fill", "none");
-
-  const fullArea = d3
-    .area()
-    .x((d) => x(d.date))
-    .y0(y(0))
-    .y1((d) => y(d.fullPct));
-
-  const fullLine = d3
-    .line()
-    .x((d) => x(d.date))
-    .y((d) => y(d.fullPct));
-
-  svg
-    .append("g")
-    .datum(data)
-    .append("path")
-    .attr("d", fullArea)
-    .attr("class", "vaccineArea")
-    .attr("id", "vaccine-full")
-    .attr("fill", color.Full)
-    .attr("fill-opacity", 0.5);
-
-  svg
-    .append("g")
-    .datum(data)
-    .append("path")
-    .attr("d", fullLine)
-    .attr("class", "vaccineLine")
-    .attr("id", "vaccineLine-full")
-    .attr("stroke", color.Full)
-    .attr("stroke-width", 5)
-    .attr("fill", "none");
-
-  const endText = (xPos, text, yPos, color, lab) => {
     svg
-      .append("text")
-      .attr("class", "vaccineEndLabel")
-      .attr("id", `endLabel-${lab}`)
-      .text(Math.round(text * 100) + "%")
-      .attr("x", x(xPos) + 5)
-      .attr("y", y(yPos))
-      .attr("fill", color)
-      .style("font-size", "16pt")
-      .style("font-weight", "bold")
-      .style("alignment-baseline", "middle");
-  };
+      .append("g")
+      .style("font-family", "Helvetica Neue,Helvetica,Arial,sans-serif")
+      .style("font-size", "12pt")
+      .attr("transform", `translate(0, ${size.height - margin.bottom + 5})`)
+      .call(
+        d3
+          .axisBottom()
+          .scale(x)
+          .tickValues([
+            ...(size.width < 600 ? [x.domain()[1]] : x.domain()),
+            ...x.ticks(),
+          ])
+          .tickFormat((d) => {
+            const t = d3.timeFormat("%b")(d);
 
-  const lastData = data[data.length - 1];
-  console.log(lastData);
-  endText(
-    lastData.date,
-    lastData.singlePct,
-    lastData.singlePct,
-    color.Partial,
-    "partial"
-  );
-  endText(
-    lastData.date,
-    lastData.fullPct,
-    lastData.fullPct,
-    color.Full,
-    "full"
-  );
-  // endText(
-  //   lastData.date,
-  //   lastData.singlePct - lastData.fullPct,
-  //   (lastData.fullPct + lastData.singlePct) / 2,
-  //   "#adadad"
-  // );
+            return t === "Jan" ? t + "'21" : t;
+          })
+      );
 
-  const legend = d3.select("#vaccineLegend").style("width", size.width);
+    const y = d3
+      .scaleLinear()
+      .domain([0, 0.6])
+      .range([size.height - margin.bottom, margin.top]);
 
-  legend
-    .append("p")
-    .style("display", "black")
-    .style("line-height", "18pt")
-    .html(
-      `County Percentage of<span class='squares'></span>and<span class='squares'></span>including adults and children.`
+    const horizLines = svg.append("g");
+
+    y.ticks(5)
+      .slice(1)
+      .forEach((yVal, i) => {
+        horizLines
+          .append("text")
+          .text(`${yVal * 100}%${i === 5 ? " of Total County Population" : ""}`)
+          .attr("fill", "#adadad")
+          .attr("x", margin.left)
+          .attr("y", y(yVal) - 5);
+
+        horizLines
+          .append("line")
+          .attr("x1", margin.left)
+          .attr("x2", size.width - margin.right)
+          .attr("y1", y(yVal))
+          .attr("y2", y(yVal))
+          .attr("stroke", "#d3d3d3")
+          .attr("stroke-width", "0.5px");
+      });
+
+    const singleArea = d3
+      .area()
+      .x((d) => x(d.date))
+      .y0((d) => y(d.fullPct))
+      .y1((d) => y(d.singlePct));
+
+    const singleLine = d3
+      .line()
+      .x((d) => x(d.date))
+      .y((d) => y(d.singlePct));
+    svg
+      .append("g")
+      .datum(data)
+      .append("path")
+      .attr("d", singleArea)
+      .attr("class", "vaccineArea")
+      .attr("id", "vaccine-partial")
+      .attr("fill", color.Partial)
+      .attr("fill-opacity", 0.5);
+
+    svg
+      .append("g")
+      .datum(data)
+      .append("path")
+      .attr("class", "vaccineLine")
+      .attr("id", "vaccineLine-partial")
+      .attr("d", singleLine)
+      .attr("stroke", color.Partial)
+      .attr("stroke-width", 5)
+      .attr("fill", "none");
+
+    const fullArea = d3
+      .area()
+      .x((d) => x(d.date))
+      .y0(y(0))
+      .y1((d) => y(d.fullPct));
+
+    const fullLine = d3
+      .line()
+      .x((d) => x(d.date))
+      .y((d) => y(d.fullPct));
+
+    svg
+      .append("g")
+      .datum(data)
+      .append("path")
+      .attr("d", fullArea)
+      .attr("class", "vaccineArea")
+      .attr("id", "vaccine-full")
+      .attr("fill", color.Full)
+      .attr("fill-opacity", 0.5);
+
+    svg
+      .append("g")
+      .datum(data)
+      .append("path")
+      .attr("d", fullLine)
+      .attr("class", "vaccineLine")
+      .attr("id", "vaccineLine-full")
+      .attr("stroke", color.Full)
+      .attr("stroke-width", 5)
+      .attr("fill", "none");
+
+    const endText = (xPos, text, yPos, color, lab) => {
+      svg
+        .append("text")
+        .attr("class", "vaccineEndLabel")
+        .attr("id", `endLabel-${lab}`)
+        .text(Math.round(text * 100) + "%")
+        .attr("x", x(xPos) + 5)
+        .attr("y", y(yPos))
+        .attr("fill", color)
+        .style("font-size", "16pt")
+        .style("font-weight", "bold")
+        .style("alignment-baseline", "middle");
+    };
+
+    const lastData = data[data.length - 1];
+    console.log(lastData);
+    endText(
+      lastData.date,
+      lastData.singlePct - lastData.fullPct,
+      lastData.singlePct,
+      color.Partial,
+      "partial"
     );
+    endText(
+      lastData.date,
+      lastData.fullPct,
+      lastData.fullPct,
+      color.Full,
+      "full"
+    );
+    // endText(
+    //   lastData.date,
+    //   lastData.singlePct - lastData.fullPct,
+    //   (lastData.fullPct + lastData.singlePct) / 2,
+    //   "#adadad"
+    // );
 
-  legend
-    .selectAll(".squares")
-    .data(["Partial", "Full"])
-    .style("text-transform", "capitalize")
-    .style("text-decoration", "underline")
-    .style("text-decoration-color", (d) => color[d])
-    .style("text-decoration-thickness", "0.2em")
-    .style("text-underline-offset", "0.2em")
-    .attr("class", "vaccineLegends")
-    .text((d) => `${d}${d === "Full" ? "" : "l"}y Vaccinated`)
-    .style("padding", "5px")
-    .style("-webkit-user-select", "none")
-    .style("user-select", "none")
-    .on("mouseover", function (event, d) {
-      d3.selectAll(".vaccineLegends").style("opacity", 0.2);
-      d3.selectAll(".vaccineLine").style("stroke-width", 0);
-      d3.selectAll(".vaccineEndLabel").style("fill-opacity", 0.2);
+    const legend = d3.select("#vaccineLegend").style("width", size.width);
 
-      if (d.toLowerCase() === "partial") {
-        d3.selectAll(".vaccineArea").style("fill", color.Partial);
-        d3.selectAll(".vaccineEndLabel").style("fill-opacity", 0);
-      } else {
+    legend.selectAll("*").remove();
+
+    legend
+      .append("p")
+      .style("display", "black")
+      .style("line-height", "18pt")
+      .html(
+        `County Percentage of<span class='squares'></span>and<span class='squares'></span>including adults and children.`
+      );
+
+    legend
+      .selectAll(".squares")
+      .data(["Partial", "Full"])
+      .style("text-transform", "capitalize")
+      .style("text-decoration", "underline")
+      .style("text-decoration-color", (d) => color[d])
+      .style("text-decoration-thickness", "0.2em")
+      .style("text-underline-offset", "0.2em")
+      .attr("class", "vaccineLegends")
+      .text((d) => `${d}${d === "Full" ? "" : "l"}y Vaccinated`)
+      .style("padding", "5px")
+      .style("-webkit-user-select", "none")
+      .style("user-select", "none")
+      .on("mouseover touchstart", function (event, d) {
+        d3.selectAll(".vaccineLegends").style("opacity", 0.2);
+        d3.selectAll(".vaccineLine").style("stroke-width", 0);
+        d3.selectAll(".vaccineEndLabel").style("fill-opacity", 0.2);
         d3.selectAll(".vaccineArea").style("fill-opacity", 0.2);
-      }
 
-      d3.select(this).style("opacity", 1);
-      d3.select(`#vaccine-${d.toLowerCase()}`).style("fill-opacity", 0.5);
-      d3.select(`#vaccineLine-${d.toLowerCase()}`).style("stroke-width", 5);
-      d3.select(`#endLabel-${d.toLowerCase()}`).style("fill-opacity", 1);
-    })
-    .on("mouseleave", (_, d) => {
-      if (d.toLowerCase() === "partial") {
-        d3.selectAll("#vaccine-full").style("fill", color.Full);
-      }
+        d3.select(this).style("opacity", 1);
+        d3.select(`#vaccine-${d.toLowerCase()}`).style("fill-opacity", 0.5);
+        d3.select(`#vaccineLine-${d.toLowerCase()}`).style("stroke-width", 5);
+        d3.select(`#endLabel-${d.toLowerCase()}`).style("fill-opacity", 1);
+      })
+      .on("mouseleave touchend touchcancel", () => {
+        d3.selectAll(".vaccineLegends").style("opacity", 1);
+        d3.selectAll(".vaccineLine").style("stroke-width", 5);
+        d3.selectAll(".vaccineEndLabel").style("fill-opacity", 1);
+        d3.selectAll(".vaccineArea").style("fill-opacity", 0.5);
+      });
 
-      d3.selectAll(".vaccineLegends").style("opacity", 1);
-      d3.selectAll(".vaccineLine").style("stroke-width", 5);
-      d3.selectAll(".vaccineEndLabel").style("fill-opacity", 1);
-      d3.selectAll(".vaccineArea").style("fill-opacity", 0.5);
-    });
+    // svg.on("mousemove", function (event) {
+    //   const mouse = d3.pointer(event);
 
-  // svg.on("mousemove", function (event) {
-  //   const mouse = d3.pointer(event);
+    //   const date = x.invert(mouse[0]);
+    //   const bisect = d3.bisector((d) => d.date).right;
+    //   const idx = bisect(d.values, date);
 
-  //   const date = x.invert(mouse[0]);
-  //   const bisect = d3.bisector((d) => d.date).right;
-  //   const idx = bisect(d.values, date);
-
-  //   console.log(idx);
-  // });
+    //   console.log(idx);
+    // });
+  };
+  makePlot();
 })();
--- plots/countyVariants.js
+++ plots/countyVariants.js
@@ -19,147 +19,256 @@
     });
     return { date: parseTime(date), ...variants };
   });
-  const variants = ["epsilon", "iota", "gamma", "delta", "alpha"];
-  // const variants = shuffleArray(Object.keys(data[0]).slice(1));
+  const variants = ["epsilon", "alpha", "delta", "iota", "gamma"];
 
-  const size = {
-    width: Math.min(600, window.innerWidth * 0.95),
-    height: 400,
-  };
+  const color = d3
+    .scaleOrdinal()
+    .domain(variants)
+    .range(["#4e79a7", "#59a14f", "#76b7b2", "#f28e2c", "#e15759"]);
 
-  const margin = {
-    top: 40,
-    bottom: 40,
-    left: 20,
-    right: 20,
-  };
-  const svg = d3.select("#variantPlot");
+  window.addEventListener("resize", () => {
+    makePlot();
+  });
 
-  svg.attr("width", size.width).attr("height", size.height);
+  const makePlot = () => {
+    const size = {
+      width: Math.min(600, window.innerWidth * 0.95),
+      height: 400,
+    };
 
-  const x = d3
-    .scaleTime()
-    .domain(d3.extent(data, (d) => d.date))
-    .range([margin.left, size.width - margin.right]);
+    const margin = {
+      top: 40,
+      bottom: 40,
+      left: window.innerWidth > 500 ? 60 : 40,
+      right: window.innerWidth > 500 ? 60 : 40,
+    };
+    const svg = d3.select("#variantPlot");
 
-  svg
-    .append("g")
-    .style("font-size", "12pt")
-    .attr("transform", `translate(0, ${size.height - margin.bottom + 5})`)
-    .call(d3.axisBottom().scale(x).ticks(6).tickFormat(d3.timeFormat("%b")));
+    svg.selectAll("*").remove();
 
-  const y = d3
-    .scaleLinear()
-    .domain([0, 250])
-    .range([size.height - margin.bottom, margin.top]);
+    svg.attr("width", size.width).attr("height", size.height);
 
-  const horizLines = svg.append("g");
+    const x = d3
+      .scaleTime()
+      .domain(d3.extent(data, (d) => d.date))
+      .range([margin.left, size.width - margin.right]);
 
-  y.ticks(5)
-    .slice(1)
-    .forEach((yVal, i) => {
-      horizLines
-        .append("text")
-        .text(yVal + (yVal === 250 ? " Cases" : ""))
-        .attr("fill", "#adadad")
-        .attr("x", margin.left)
-        .attr("y", y(yVal) - 5);
+    svg
+      .append("g")
+      .style("font-size", "12pt")
+      .attr("transform", `translate(0, ${size.height - margin.bottom + 5})`)
+      .call(
+        d3
+          .axisBottom()
+          .scale(x)
+          // .ticks(window.innerWidth > 400 ? 6 : 4)
+          .tickValues(
+            data
+              .map(({ date }, i) =>
+                window.innerWidth > 400 ? date : i % 2 == 1 ? null : date
+              )
+              .filter((d) => d !== null)
+          )
+          .tickFormat((d) => {
+            const t = d3.timeFormat("%b")(d);
 
-      horizLines
-        .append("line")
-        .attr("x1", margin.left)
-        .attr("x2", size.width - margin.right)
-        .attr("y1", y(yVal))
-        .attr("y2", y(yVal))
-        .attr("stroke", "#d3d3d3")
-        .attr("stroke-width", "0.5px");
+            return t === "Jan" ? t + "'21" : t;
+          })
+      );
+
+    const y = d3
+      .scaleLinear()
+      // .domain([0, 250])
+      .range([size.height - margin.bottom, margin.top]);
+
+    const horizLines = svg.append("g");
+
+    y.ticks(5)
+      // .slice(1)
+      .forEach((yVal, i) => {
+        horizLines
+          .append("text")
+          .text(yVal * 100 + (yVal === 1 ? "%" : ""))
+          .attr("fill", "#adadad")
+          .attr("x", 0)
+          .attr("y", y(yVal) - 5);
+
+        horizLines
+          .append("line")
+          .attr("x1", 0)
+          .attr("x2", size.width)
+          .attr("y1", y(yVal))
+          .attr("y2", y(yVal))
+          .attr("stroke", "#d3d3d3")
+          .attr("stroke-width", "0.5px");
+      });
+
+    // const stackedData = d3.stack().offset(d3.stackOffsetNone).keys(variants)(
+    //   data
+    // );
+    const dataNormalized = data.map((d) => {
+      const output = { date: d.date };
+
+      let tot = 0;
+      variants.forEach((v) => {
+        tot += d[v];
+      });
+
+      variants.forEach((v) => {
+        output[v] = d[v] / tot;
+      });
+
+      return output;
     });
 
-  const color = d3.scaleOrdinal().domain(variants).range(d3.schemeTableau10);
+    const stackedData = d3.stack().keys(variants)(dataNormalized);
 
-  const stackedData = d3.stack().offset(d3.stackOffsetNone).keys(variants)(
-    data
-  );
+    // const separatedData = {};
+    // dataNormalized.forEach((d) => {
+    //   variants.forEach((v) => {
+    //     if (d[v] === 0) {
+    //       return;
+    //     }
 
-  const area = d3
-    .area()
-    .x((d) => x(d.data.date))
-    .y0((d) => y(d[0]))
-    .y1((d) => y(d[1]))
-    .curve(d3.curveMonotoneX);
+    //     if (Object.keys(separatedData).includes(v)) {
+    //       separatedData[v].push({ name: v, date: d.date, pct: d[v] });
+    //     } else {
+    //       separatedData[v] = [{ name: v, date: d.date, pct: d[v] }];
+    //     }
+    //   });
+    // });
 
-  svg
-    .append("g")
-    .selectAll("mylayers")
-    .data(stackedData)
-    .enter()
-    .append("path")
-    .attr("class", "variants")
-    .attr("id", (d) => `variant-${d.key}`)
-    .attr("fill-opacity", 1)
-    .attr("fill", function (d) {
-      return color(d.key);
-    })
-    .attr("d", area);
+    const rectWidth =
+      (size.width - margin.left - margin.right) /
+      (window.innerWidth > 500 ? 12 : 10);
 
-  const legend = d3
-    .select("#variantLegend")
-    .style("width", size.width)
-    .style("display", "flex")
-    .style("flex-wrap", "wrap");
+    console.log(dataNormalized);
 
-  legend
-    .selectAll("leg")
-    .data(variants.sort())
-    .enter()
-    .append("div")
-    .attr("class", "variantLegends")
-    .html(
-      (d) =>
-        `<div style="display: flex; text-transform: capitalize;">
+    svg
+      .append("g")
+      .selectAll("mylayers")
+      .data(stackedData)
+      .enter()
+      // .append("path")
+      // .attr("class", "variants")
+      // .attr("id", (d) => `variant-${d.key}`)
+      // .attr("fill-opacity", 0.7)
+      // .attr("fill", function (d) {
+      //   return color(d.key);
+      // })
+      // .attr("d", area)
+      .append("g")
+      .attr("class", "variants")
+      .attr("id", (d) => `variant-${d.key}`)
+      .attr("fill", function (d) {
+        return color(d.key);
+      })
+      .selectAll("rect")
+      .data((d) => d)
+      .enter()
+      .append("rect")
+      .attr("y", (d) => y(d[1]))
+      .attr("x", (d) => x(d.data.date) - rectWidth / 2)
+      .attr("width", rectWidth)
+      .attr("height", (d) => y(d[0]) - y(d[1]));
+
+    svg
+      .append("g")
+      .selectAll(".variantOverlay")
+      .data(data.map(({ date }) => date))
+      .enter()
+      .append("rect")
+      .attr("y", y(1))
+      .attr("x", (d) => x(d) - rectWidth / 2)
+      .attr("width", rectWidth)
+      .attr("height", y(0) - y(1))
+      .attr("fill-opacity", 0)
+      .on("mouseover", (_, d) => {
+        console.log(data.find(({ date }) => date === d));
+      });
+    // const variantLine = d3
+    //   .line()
+    //   .y((d) => y(d.pct))
+    //   .x((d) => x(d.date));
+
+    // console.log(Object.values(separatedData));
+    // svg
+    //   .append("g")
+    //   .selectAll(".variantLines")
+    //   .data(Object.values(separatedData))
+    //   .enter()
+    //   .append("path")
+    //   .attr("d", (d) => {
+    //     console.log(d);
+    //     return variantLine(d);
+    //   })
+    //   .attr("fill", "none")
+    //   .attr("stroke", (d) => color(d[0].name))
+    //   .attr("stroke-width", 5);
+
+    const legend = d3
+      .select("#variantLegend")
+      .style("width", size.width)
+      .style("display", "flex")
+      .style("flex-wrap", "wrap");
+
+    legend.selectAll("*").remove();
+
+    legend
+      .selectAll("leg")
+      .data(variants.sort())
+      .enter()
+      .append("div")
+      .attr("class", "variantLegends")
+      .html(
+        (d) =>
+          `<div style="display: flex; text-transform: capitalize;">
           <div style="width: 20px; height: 20px; margin-right: 3px;background-color: ${color(
             d
           )}"></div>
           ${d}
         </div>`
-    )
-    .style("padding", "5px")
-    .style("user-select", "none")
-    .style("-webkit-user-select", "none")
-    .on("mouseover", function (event, d) {
-      d3.selectAll(".variantLegends").style("opacity", 0.2);
-      d3.selectAll(".variants").style("fill-opacity", 0.2);
-      d3.select(this).style("opacity", 1);
-      d3.select(`#variant-${d}`).style("fill-opacity", 1);
+      )
+      .style("padding", "5px")
+      .style("user-select", "none")
+      .style("-webkit-user-select", "none")
+      .on("mouseover touchstart", function (event, d) {
+        d3.selectAll(".variantLegends").style("opacity", 0.2);
+        d3.selectAll(".variants").style("fill-opacity", 0.2);
+        d3.select(this).style("opacity", 1);
+        d3.select(`#variant-${d}`).style("fill-opacity", 1);
 
-      const varData = stackedData
-        .find((v) => v.key === d)
-        .filter(([a, b]) => a !== b);
+        const varData = stackedData
+          .find((v) => v.key === d)
+          .filter(([a, b]) => a !== b);
 
-      svg
-        .selectAll(".points")
-        .data(varData)
-        .enter()
-        .append("circle")
-        .attr("class", "points")
-        .attr("cx", (d) => x(d.data.date))
-        .attr("cy", (v) => y(v[1]))
-        .attr("r", 4);
-      svg
-        .selectAll(".pointLabels")
-        .data(varData)
-        .enter()
-        .append("text")
-        .attr("class", "pointLabels")
-        .attr("x", (d) => x(d.data.date))
-        .attr("y", (v) => y(v[1]) - 8)
-        .text((v) => v[1] - v[0])
-        .attr("text-anchor", "middle");
-    })
-    .on("mouseleave", () => {
-      d3.selectAll(".variantLegends").style("opacity", 1);
-      d3.selectAll(".variants").style("fill-opacity", 1);
-      d3.selectAll(".points").remove();
-      d3.selectAll(".pointLabels").remove();
-    });
+        svg
+          .selectAll(".points")
+          .data(varData)
+          .enter()
+          .append("circle")
+          .attr("class", "points")
+          .attr("cx", (d) => x(d.data.date))
+          .attr("cy", (v) => y(v[1]))
+          .attr("r", 4);
+        svg
+          .selectAll(".pointLabels")
+          .data(varData)
+          .enter()
+          .append("text")
+          .attr("class", "pointLabels")
+          .attr("x", (d) => x(d.data.date))
+          .attr("y", (v) => y(v[1]) - 8)
+          .text((v) => Math.round((v[1] - v[0]) * 100) + "%")
+          .attr("text-anchor", "middle");
+      })
+      .on("mouseleave touchend touchcancel", () => {
+        d3.selectAll(".variantLegends").style("opacity", 1);
+        d3.selectAll(".variants").style("fill-opacity", 1);
+        d3.selectAll(".points").remove();
+        d3.selectAll(".pointLabels").remove();
+      });
+  };
+
+  makePlot();
 })();
